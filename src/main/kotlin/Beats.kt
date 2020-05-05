180ebdde92720584e78e18a1c5160282723de1c9
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-05 13:54:53 +0300

    add fun playSound, playBeats

--- src/main/kotlin/Beats.kt
+++ src/main/kotlin/Beats.kt
@@ -1,3 +1,31 @@
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
 class Beats {
 
+    fun playBeats(beats: String, file: String){
+        val parts = beats.split("x")
+        var count = 0
+        for (part in parts){
+            count += part.length + 1
+            if (part == ""){
+                playSound(file)
+            } else{
+                Thread.sleep(100*(part.length + 1L))
+                if (count < beats.length){
+                    playSound(file)
+                }
+            }
+        }
+    }
+    fun playSound(file: String){
+        val clip = AudioSystem.getClip()
+        val audioInputStream = AudioSystem.getAudioInputStream(
+            File(
+                file
+            )
+        )
+        clip.open(audioInputStream)
+        clip.start()
+    }
 }
