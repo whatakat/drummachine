1d44c173e86004eabc95e141fc4a7a02e29bab00
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-05 14:37:18 +0300

    add suspend

--- src/main/kotlin/Beats.kt
+++ src/main/kotlin/Beats.kt
@@ -1,10 +1,11 @@
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import javax.sound.sampled.AudioSystem
 
-   fun playBeats(beats: String, file: String){
+  suspend fun playBeats(beats: String, file: String){
         val parts = beats.split("x")
         var count = 0
         for (part in parts){
@@ -12,7 +13,7 @@ import javax.sound.sampled.AudioSystem
             if (part == ""){
                 playSound(file)
             } else{
-                Thread.sleep(100*(part.length + 1L))
+                delay(100*(part.length + 1L))
                 if (count < beats.length){
                     playSound(file)
                 }
@@ -31,7 +32,7 @@ import javax.sound.sampled.AudioSystem
     }
 
 
-fun main(){
+suspend fun main(){
     runBlocking {launch {  playBeats("x-x-x-x-x-x-","toms.aiff")}
         playBeats("x-----x-----","crash_cymbal.aiff")  }
 }
